8fb2f696f4e31978d6b82a3283d1a3d4089ba101
Naz1337 <cc20146@stdmail.ump.edu.my>
2023-01-02 21:44:13 +0800

    swapped Client to Bot and new command `add`

--- main.py
+++ main.py
@@ -2,44 +2,61 @@ import os
 import logging
 
 import discord
+from discord.ext import commands
 from discord import app_commands
 from dotenv import load_dotenv
 
 
-@app_commands.command()
-async def ping(interaction: discord.Interaction):
+@commands.hybrid_command()
+async def ping(ctx: commands.Context):
     """Tell you the latency of this bot"""
-    await interaction.response.send_message(f"Pong {int(interaction.client.latency * 1000)}ms!", ephemeral=True)
+    latency = int(ctx.bot.latency * 1000)
+    await ctx.send(f"Pong {latency}ms", ephemeral=True)
+    logging.info(
+        f"{ctx.author} used ping and the current latency is {latency}ms")
 
 
-class MyClient(discord.Client):
-    MY_GUILD = discord.Object(id=347361743585935361)
-    def __init__(self, intents: discord.Intents) -> None:
-        super().__init__(intents=intents)
+@commands.hybrid_command()
+@app_commands.describe(a='The first number', b='The second number')
+async def add(ctx: commands.Context, a: int = commands.parameter(description='The first number'), b: int = commands.parameter(description='The second number')):
+    """Add the two number that was given"""
+    answer = f"{a} + {b} = {a + b} !"
+    await ctx.send(answer, ephemeral=True)
+    logging.info(f"{ctx.author} used add command: {answer}")
 
-        self.tree = app_commands.CommandTree(self)
-    
-    async def setup_hook(self) -> None:
-        self.tree.copy_global_to(guild=self.MY_GUILD)
-        await self.tree.sync(guild=self.MY_GUILD)
-    
-    async def on_ready(self):
-        logging.info(f"Logged in as {self.user} (ID: {self.user.id}), bot is ready!")
-    
-    
 
-if __name__ == "__main__":
-    load_dotenv()
+def main():
+    intents = discord.Intents.default()
+    intents.message_content = True
+    bot = commands.Bot(
+        'p!', description="The bot that handle your blackjack games", intents=intents)
 
-    logger = logging.getLogger()
+    logger = logging.getLogger()  # root
     dt_fmt = '%Y-%m-%d %H:%M:%S'
-    formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+    formatter = logging.Formatter(
+        '[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
     file_handler = logging.FileHandler('logs.txt')
     file_handler.setFormatter(formatter)
     file_handler.setLevel(logging.DEBUG)
     logger.addHandler(file_handler)
 
-    token: str = os.environ.get('dsc_key')
-    client = MyClient(discord.Intents.default())
-    client.tree.add_command(ping)
-    client.run(token, root_logger=True)
+    load_dotenv()
+    token = os.environ.get('dsc_key')
+
+    bot.add_command(ping)
+    bot.add_command(add)
+
+    async def on_ready():
+        logging.info(
+            f"Bot connected as {bot.user} (ID: {bot.user.id}), ready!")
+        logging.info("calling .sync on the bot tree")
+        await bot.tree.sync()
+
+    bot.add_listener(on_ready, 'on_ready')
+
+    # run setup the console formatting logging stuff
+    bot.run(token, log_level=logging.INFO, root_logger=True)
+
+
+if __name__ == "__main__":
+    main()
