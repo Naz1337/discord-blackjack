5abaa041d234e11384edb945d3d8494310abcb02
Naz1337 <cc20146@stdmail.ump.edu.my>
2023-01-03 22:16:59 +0800

    deposit command

--- cogs/blackjack_cog.py
+++ cogs/blackjack_cog.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from discord import app_commands, interactions
 import discord
 import logging
 import aiosqlite
@@ -18,7 +19,8 @@ class BlackJack(commands.Cog):
                 """
                 CREATE TABLE IF NOT EXISTS player (
                     player_id INTEGER PRIMARY KEY,
-                    cash REAL
+                    balance REAL,
+                    deposit REAL
                 )
                 """)
 
@@ -26,29 +28,50 @@ class BlackJack(commands.Cog):
 
     @commands.command()
     @commands.is_owner()
-    async def add_money(self, ctx: commands.Context, amount: int, to: discord.User = commands.Author):
-        """add money to the person you choose"""
-        
+    async def add_money(self, ctx: commands.Context, amount: float, to: discord.User = commands.Author):
+        """add money to the person you choose without increasing deposit"""
         
         async with aiosqlite.connect(self.DB_FILENAME) as db:
             await db.execute(
                 """
-                    INSERT OR REPLACE INTO player (player_id, cash)
-                    VALUES (?, ?)
+                    INSERT INTO player (player_id, balance, deposit)
+                    VALUES (?, ?, ?)
                     ON CONFLICT (player_id) DO
-                        UPDATE SET cash = cash + excluded.cash;
+                        UPDATE SET balance = balance + excluded.balance;
                 """,
-                (to.id, amount))
+                (to.id, amount, 0))
             await db.commit()
 
-            async with db.execute('SELECT * FROM player WHERE player_id = ?', (to.id,)) as cur:
+            async with db.execute('SELECT balance, deposit FROM player WHERE player_id = ?', (to.id,)) as cur:
                 async for row in cur:
-                    cash = row[1]  # cash sit at the second column
+                    balance, deposit = row
 
-            resp = f"Added ${amount} to {to}(${cash})"
+            resp = f"Added ${amount:,.2f} to {to}(${balance:,.2f}) without increasing their current deposit(${deposit:,.2f})"
 
             await ctx.send(resp, ephemeral=True)
             logger.info(resp)
+    
+    @app_commands.command()
+    async def deposit(self, interaction: interactions.Interaction, amount: float):
+        async with aiosqlite.connect(self.DB_FILENAME) as db:
+            await db.execute(
+                """
+                INSERT INTO player (player_id, balance, deposit)
+                VALUES (?, ?, ?)
+                ON CONFLICT (player_id) DO UPDATE
+                SET balance = balance + excluded.balance,
+                    deposit = deposit + excluded.deposit;
+                """,
+                (interaction.user.id, amount, amount))
+            await db.commit()
+
+            async with db.execute('SELECT balance, deposit FROM player WHERE player_id = ?', (interaction.user.id,)) as cur:
+                async for row in cur:
+                    balance, deposit = row
+            
+            resp = f"Deposited ${amount:,.2f} to your balance(${balance:,.2f}). _Total Deposit: ${deposit:,.2f}_"
+            await interaction.response.send_message(resp, ephemeral=True)
+            logger.info(f"{resp} to {interaction.user}")
 
 
 async def setup(bot: commands.Bot):
