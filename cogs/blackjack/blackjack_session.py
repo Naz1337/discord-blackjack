c689592c64b13256624c72c58541209085227017
Naz1337 <cc20146@stdmail.ump.edu.my>
2023-01-07 01:35:30 +0800

    expand on the idea for this stuff

--- cogs/blackjack/blackjack_session.py
+++ cogs/blackjack/blackjack_session.py
@@ -14,3 +14,9 @@ class BlackJackSession:
 
     # add attribute for players card, players turn
     # should this place run the discord bot?
+    # think of it like game state, we can have the
+    # function for every option like hit and stand,
+    # split and stuff, each function will check it
+    # game state to see if the the valid move
+    # and check who turn it is and update their hand
+    # in the this class
